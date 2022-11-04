7e4a24c2bdfbf8802de2dffcb969786c4518cc9e
Zayn Rekhi <zaynrekhi@Zayns-MacBook-Pro.local>
2022-11-04 16:20:40 -0400

    updates

--- include/main.h
+++ include/main.h
@@ -63,6 +63,12 @@
 extern "C" {
 #endif
 void autonomous(void);
+void left_side(void);
+void right_side(void);
+void awp(void);
+
+void skills(void);
+
 void initialize(void);
 void disabled(void);
 void competition_initialize(void);
